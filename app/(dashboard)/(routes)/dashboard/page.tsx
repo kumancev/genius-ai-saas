95abd92c0e840dfeae805b10065414fbfcc68f3c
Alex Kumancev <kumancev1@gmail.com>
2023-07-23 18:52:50 +0300

    feat: complete sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from '@clerk/nextjs'
-
 const DashboardPage = () => {
   return (
     <div>
       <p>Dashboard page (Protected)</p>
-      <UserButton afterSignOutUrl="/" />
     </div>
   )
 }
